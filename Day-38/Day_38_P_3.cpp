6e4f2b8a614f0965097727cd82478412f51b45b9
Sharuk <sharuksharuk46@gmail.com>
2022-03-23 10:42:27 +0530

    Day 38(SQL)

--- Day-38/Day_38_P_3.cpp
+++ Day-38/Day_38_P_3.cpp
@@ -1,4 +1,4 @@
-'''
+/*'''
 
 You are playing a shooting game.
 There are N bottles hanging to a rod, i-th bottle numbered bottle[i].
@@ -68,3 +68,56 @@ Total = 102
 '''
 
 #Solution:
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int rep(int i,int j,int n,vector<int>& temp,vector<vector<int>>& dp){
+    
+    if(i>j){
+        return 0;
+    }
+    if(i==j){
+        int ans=temp[i];
+        if(i-1>=0){
+            ans=ans*temp[i-1];
+        }
+        if(i+1<n){
+            ans=ans*temp[i+1];
+        }
+        return ans;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    int ans=INT_MIN;
+    for(int k=i;k<=j;k++){
+        ans=max(ans,temp[k]*temp[i-1]*temp[j+1]+rep(i,k-1,n,temp,dp)+rep(k+1,j,n,temp,dp));
+        
+    }
+    return dp[i][j]=ans;
+    
+    
+}
+
+int main(){
+    
+    vector<int> temp;
+    string s;
+    getline(cin,s);
+    
+    temp.push_back(1);
+    
+    stringstream ss(s);
+    int x;
+    while(ss>>x){
+        temp.push_back(x);
+    }
+    
+    temp.push_back(1);
+    int n = temp.size();
+    
+    vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
+    cout<<rep(1,n-2,n,temp,dp)<<endl;
+}
