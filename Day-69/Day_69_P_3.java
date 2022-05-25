87f73595be3a8cecf8a319164dd846b1b1d14312
Sharuk <sharuksharuk46@gmail.com>
2022-05-25 19:42:46 +0530

    Day 69

---
+++ Day-69/Day_69_P_3.java
@@ -0,0 +1,53 @@
+/*
+
+Mr Nagireddy is working with Binary Trees.
+The elements of the tree are given in the level order format.
+
+He is given the root of the binary tree T, to find the most occured STsum(s), if 
+there are more than one STsums like that, return all of them as a list.
+An STsum is a sub tree sum from a node N, where that node N acts as the root node,
+STsum is the sum of all the nodes data of that sub tree of node N.
+
+Your task is to help Nagireddy to find and return the list of STsums, which 
+occured more number of times.
+
+Your task is to implement the class Solution:
+	- public List<Integer> findFrequentSum(BinaryTreeNode root):
+	return a list of integers as result.
+
+NOTE: The Binary Tree T may contain negative values. 	
+
+Input Format:
+-------------
+Line-1: comma separated integers, level order binary tree
+
+Output Format:
+--------------
+Print a list of integers in ascending order as result.
+
+
+Sample Input-1:
+---------------
+4,-2,5,3,1
+
+Sample Output-1:
+----------------
+[1,2,3,5,11]
+
+
+Sample Input-2:
+---------------
+4,-2,5,3,2
+
+Sample Output-2:
+----------------
+[3]
+
+
+
+*/
+
+
+public class Day_69_P_3 {
+    
+}
