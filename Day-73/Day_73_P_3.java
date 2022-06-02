b0281273d3eb8b8e6714ccb9831676b05691f5e4
Sharuk <sharuksharuk46@gmail.com>
2022-06-02 18:47:43 +0530

    Day 73

---
+++ Day-73/Day_73_P_3.java
@@ -0,0 +1,74 @@
+/*
+
+
+Vimal and Kamal are pals. They're attempting to play a word game.
+The following are the game's rules:
+- There will be two terms in the game: w1 and w2.
+- One of the words must be chosen by Kamal.
+- If Kamal picks w1, w1[i] should be greater than w2[i], for every 'i' 
+  in 0 <= i < w1.length OR vice versa, for some shuffle of w1 and w2.
+- Kamal is the winner if the preceding step is true; else, Kamal is the loser.
+
+Your task is to assist Kamal in selecting the winning word.
+If feasible, print 'winner,' if not, 'loser.'
+
+Input Format:
+-------------
+Two space separated strings, two words w1 and w2.
+
+Output Format:
+--------------
+Print a boolean result.
+
+
+Sample Input-1:
+---------------
+break class
+
+Sample Output-1:
+----------------
+winner
+
+Explanation:
+------------
+Kamal chooses w2="class", and the shuffle of w1 is "beark", 
+so that Kamal wins the word game. 
+
+
+Sample Input-2:
+---------------
+kmit ngit
+
+Sample Output-2:
+----------------
+loser
+
+
+
+*/
+
+import java.util.*;
+class Day_73_P_3{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.next();
+        String s2=sc.next();
+        char ch1[]=s1.toCharArray();
+        char ch2[]=s2.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        boolean ans1=true,ans2=true;
+        for(int i=0;i<ch1.length;i++){
+            if(ch1[i]<ch2[i])
+                ans1=false;
+        }
+        for(int i=0;i<ch2.length;i++){
+            if(ch2[i]<ch1[i])
+                ans2=false;
+        }
+        if(ans1 || ans2)
+            System.out.println("winner");
+        else
+            System.out.println("loser");
+    }
+}
